935b587bd9c38bbcc3ec7488d297d1a4e1447ecb
Kapil <kapiljavin@gmail.com>
2024-01-24 18:56:38 +0530

    udpate user session

--- auth.ts
+++ auth.ts
@@ -11,7 +11,27 @@ export const {
   signOut
 } = NextAuth({
   callbacks: {
-    
+    async signIn({ user, account, profile, email, credentials }) {
+      // console.log('signIn', { user, account, profile, email, credentials })
+
+      return true
+    },
+    // async redirect({ url, baseUrl }) {
+    //   return baseUrl
+    // },
+    async session({session, token }) {
+      session.user.userData = token?.userData
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      
+      if(!token.userData){
+        token.userData = user
+      }
+
+      return token
+      
+    }
   },
   adapter: MongoDBAdapter(clientPromise),
   session: {strategy: "jwt"},
